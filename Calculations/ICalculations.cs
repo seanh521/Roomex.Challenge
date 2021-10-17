60e79c5783ce876afd90ad94ec2d674a317f1286
Sean Harrington <seantharri05@gmail.com>
2021-10-17 11:09:43 +0100

    Model for the API is created and Calculations Interface and Class is created

---
+++ Calculations/ICalculations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Roomex.Challenge.Calculations
+{
+    interface ICalculations
+    {
+        double CalculateDistanceBetweenCoorsKm(double latA, double longA, double latB, double longB);
+        double CalculateDistanceBetweenCoorsMi(double latA, double longA, double latB, double longB);
+        double DegreeToRadian(double degree);
+        double ConvertToMi(double calculation);
+        double ConvertToKm(double calculation);
+    }
+}
