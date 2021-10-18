672039b1c4288015e0f6248a0bdb14db433dd30c
Sean Harrington <seantharri05@gmail.com>
2021-10-18 10:54:07 +0100

    Added Unit Test

---
+++ Tests/Roomex.Challenge.UnitTests/HaversineUnitTests.cs
@@ -0,0 +1,29 @@
+﻿using Roomex.Challenge.Calculations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Roomex.Challenge.Tests.Roomex.Challenge.UnitTests
+{
+    public class HaversineUnitTests
+    {
+        //[Fact]
+        public void IsCalculationCorrectInKm_GivenSpecificInput_ReturnsTrue()
+        {
+            //Arrange
+            HaversineCalculation haversineCalculation = new HaversineCalculation();
+            double givenLatA = 53.297975;
+            double givenLongA = -6.372663;
+            double givenLatB = 41.385101;
+            double givenLongB = -81.440440;
+            double expectedResult = 5536.34;
+
+            //Act
+            double result = haversineCalculation.CalculateDistanceBetweenCoorsKm(givenLatA, givenLongA, givenLatB, givenLongB);
+
+            //Assert
+            //Assert.Equal(expectedResult, result);
+        }
+    }
+}
